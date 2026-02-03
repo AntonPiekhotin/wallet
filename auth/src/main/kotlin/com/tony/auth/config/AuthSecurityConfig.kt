6397872238373ccfb467090a79149ad8ba1430a2
apiekhotin <apiekhotin@casgate.io>
2026-02-03 17:51:08 +0100

    fixed auth security config

--- auth/src/main/kotlin/com/tony/auth/config/AuthSecurityConfig.kt
+++ auth/src/main/kotlin/com/tony/auth/config/AuthSecurityConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
@@ -12,7 +14,19 @@ class AuthSecurityConfig {
 
     @Bean
     fun chain(http: ServerHttpSecurity): SecurityWebFilterChain =
-        http.csrf { it.disable() }
-            .authorizeExchange { it.anyExchange().permitAll() }
+        http
+            .csrf { it.disable() }
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .authorizeExchange { exchange ->
+                exchange
+                    .pathMatchers("/api/v1/auth/**").permitAll()
+                    .anyExchange().authenticated()
+            }
             .build()
+
+    @Bean
+    fun noUserDetails(): ReactiveUserDetailsService {
+        return MapReactiveUserDetailsService(emptyMap())
+    }
 }
