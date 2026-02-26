757f0734dc38637aeea3cd94dea95e8c3c6b36e3
apiekhotin <apiekhotin@casgate.io>
2026-02-26 17:19:49 +0100

    added Kafka listener for handling compensation events in transaction service

--- transaction/src/main/kotlin/com/tony/mywallet/transaction/input/event/TransactionEventConsumer.kt
+++ transaction/src/main/kotlin/com/tony/mywallet/transaction/input/event/TransactionEventConsumer.kt
@@ -1,8 +1,37 @@
 package com.tony.mywallet.transaction.input.event
 
-import org.springframework.stereotype.Component
+import com.tony.common.model.constant.KafkaGroup.TRANSACTION_SERVICE
+import com.tony.common.model.constant.KafkaTopic.COMPENSATION
+import com.tony.common.model.event.SagaCompensationEvent
+import com.tony.common.saga.handler.CompensationHandler
+import com.tony.common.saga.handler.SagaDispatcher
+import com.tony.mywallet.common.jpa.store.SagaStore
+import com.tony.mywallet.transaction.service.TransactionService
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Service
 
-@Component
-class TransactionEventConsumer {
+@Service
+class UserEventConsumer(
+    private val transactionService: TransactionService,
+    handlers: List<CompensationHandler>,
+    private val sagaStore: SagaStore
+) {
+    private val dispatcher = SagaDispatcher(handlers)
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
+    @KafkaListener(
+        topics = [COMPENSATION],
+        groupId = TRANSACTION_SERVICE,
+        containerFactory = "kafkaListenerContainerFactory",
+    )
+    fun onCompensation(event: SagaCompensationEvent, acknowledgment: Acknowledgment) {
+        logger.debug("Received compensation event: {}", event)
+        try {
+            dispatcher.dispatch(event)
+        } finally {
+            acknowledgment.acknowledge()
+        }
+    }
 }
